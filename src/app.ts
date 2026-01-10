b26a4964f48d96a14a83429c7bf7d63e5aec85d9
cyberlyadnan <cyberlyadnan@gmail.com>
2026-01-10 16:18:21 +0530

    Increase JSON and URL-encoded request body size limits to 5MB for improved data handling in the application.

--- src/app.ts
+++ src/app.ts
@@ -67,8 +67,8 @@ app.use(
 );
 app.use(cors(corsOptions));
 app.use(morgan(config.app.env === 'production' ? 'combined' : 'dev'));
-app.use(express.json({ limit: '1mb' }));
-app.use(express.urlencoded({ extended: true, limit: '1mb' }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 app.use(cookieParser());
 app.use((_req, _res, next) => {
   if (_req.body) {
